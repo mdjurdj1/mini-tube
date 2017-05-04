1faf3a7e4236a87edf8261da0abee00ab0418a67
Mdjurdj1 <github email address>
2017-05-04 17:19:58 -0400

    Added redux/react dependancies to index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { browserHistory } from 'react-router'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 import './index.css';
 
 ReactDOM.render(
